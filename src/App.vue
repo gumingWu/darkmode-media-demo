01b835df2e91eb1f0796f930a867d8d2084784f7
gumingWu <15626185054@163.com>
2022-05-17 11:40:26 +0800

    按钮控制深色模式完成

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from "./components/HelloWorld.vue";
 import { computed } from "vue";
-import { useDarkMode } from "./hooks/useDarkMode";
+import { useDark, useToggle } from "@vueuse/core";
 
-const { isDark, toggleDark } = useDarkMode();
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 
 const btnMsg = computed(() => {
   return isDark.value ? "深色" : "亮色";
@@ -18,9 +19,9 @@ const btnMsg = computed(() => {
   <button @click="toggleDark()">{{ btnMsg }}</button>
 </template>
 
-<style>
+<style lang="less">
 /* 这里引入样式 */
-@import "./assets/base.css";
+@import "./assets/base.less";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,18 +1,16 @@
 import { useDark, useToggle } from "@vueuse/core";
 
 export const useDarkMode = () => {
-  const isDark = useDark({
-    storageKey: "vueuse-color-scheme",
-  });
+  const isDark = useDark();
   const toggleDark = useToggle(isDark);
 
   // 给media添加change事件，发生变化时会触发
   // 经测试，如果不加这个，当按钮显示为深色时，切换系统颜色，并不会改变按钮字体
-  const query = window.matchMedia(`(prefers-color-scheme: dark)`);
-  query.onchange = (e) => {
-    const mediaMatches = (e.target as MediaQueryList).matches;
-    isDark.value = mediaMatches;
-  };
+  // const query = window.matchMedia(`(prefers-color-scheme: dark)`);
+  // query.onchange = (e) => {
+  //   const mediaMatches = (e.target as MediaQueryList).matches;
+  //   isDark.value = mediaMatches;
+  // };
 
   return {
     isDark,
